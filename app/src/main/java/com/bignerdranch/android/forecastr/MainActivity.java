53096069f300833eb2c76399dd7e9fc889652216
Jon <jo0748gr-s@student.lu.se>
2019-08-12 14:43:04 +0200

    Autocomplete searchview
    Implemented Autocompleted searchbox as well as adding API key for Google places.

--- app/src/main/java/com/bignerdranch/android/forecastr/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/forecastr/MainActivity.java
@@ -1,14 +1,64 @@
 package com.bignerdranch.android.forecastr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    PlacesClient mPlacesClient;
+    final String TAG = "forecastr.MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "AIzaSyDt18yLpPPTSCkI26bimMNjl0mGvUcnd6s";
+
+        if (!Places.isInitialized()){
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+
+        mPlacesClient = Places.createClient(this);
+
+        final AutocompleteSupportFragment autocompleteSupportFragment =
+                (AutocompleteSupportFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.autocomplete_fragment);
+
+        //Sets the place fields when the user clicks on an autocompleted field. ie, selecting Stockholm sets all fields to sthlm-variables.
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID,Place.Field.LAT_LNG,Place.Field.NAME));
+
+
+        //Listener for selecting new place.
+        autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                final LatLng latLng = place.getLatLng();
+                Log.i(TAG, "onPlaceSelected: "+ latLng.latitude + "\n" + latLng.longitude);
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+
+            }
+        });
+
+
+
     }
 }
